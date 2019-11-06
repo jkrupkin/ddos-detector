b98de92006daab4495f5129f289038c44aa590a6
Jacob Krupkin <jkrupkin@gmail.com>
2019-11-06 14:49:15 +0000

    Added sniffer methods

--- sniffer.py
+++ sniffer.py
@@ -1,11 +1,11 @@
-
+from scapy.all import *
 port = 0
-
+t = AsyncSniffer()
 def start_sniffer():
-
+    t.start()
 def stop_sniffer():
-
+    t.stop()
 def analysis_output():
-
+    pass
 def database_output():
-
+    pass
